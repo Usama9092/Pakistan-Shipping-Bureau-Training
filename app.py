d1b403c69e784bf40a1f44cea92a310a682ee13d
Usama9092 <usama.saleem@psbureau.org>
2026-09-15 16:45:04 +0500

    Apply professional PSB theme across entire portal

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-from psb_app.main import main
+import psb_app.main as _app
+from psb_app.professional_theme import apply_professional_theme
+
+
+_base_style = _app.apply_style
+
+
+def _professional_style() -> None:
+    _base_style()
+    apply_professional_theme()
+
+
+_app.apply_style = _professional_style
+
 
 if __name__ == "__main__":
-    main()
+    _app.main()
