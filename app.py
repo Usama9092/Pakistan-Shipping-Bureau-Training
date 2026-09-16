6bc680c109285750cae071f238a69924375a258b
Usama9092 <usama.saleem@psbureau.org>
2026-09-16 15:02:50 +0500

    Run controlled source-backed curriculum publisher at startup

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import psb_app.main as _app
 import psb_app.private_video_streaming  # noqa: F401
 from psb_app.professional_theme import apply_professional_theme
+from psb_app.services.auto_publish_curriculum import prepare_and_publish_source_backed_curriculum
 
 
 _base_style = _app.apply_style
@@ -15,4 +16,5 @@ _app.apply_style = _professional_style
 
 
 if __name__ == "__main__":
+    prepare_and_publish_source_backed_curriculum()
     _app.main()
