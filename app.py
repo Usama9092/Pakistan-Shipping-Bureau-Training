31851245f2b6840b2b15b3529ebd002383e7c9c2
Usama9092 <usama.saleem@psbureau.org>
2026-09-17 10:14:34 +0500

    Use fast assessment bank maintenance

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import psb_app.main as _app
 import psb_app.private_video_streaming  # noqa: F401
 from psb_app.professional_theme import apply_professional_theme
 from psb_app.services.auto_publish_curriculum import prepare_and_publish_source_backed_curriculum
-from psb_app.services.professional_assessment_bank import ensure_professional_assessment_banks
+from psb_app.services.professional_assessment_bank_v2 import ensure_professional_assessment_banks_v2
 
 
 _base_style = _app.apply_style
@@ -20,5 +20,5 @@ if __name__ == "__main__":
     # Ensure the persistent schema is ready before controlled curriculum maintenance.
     _app.init_db()
     prepare_and_publish_source_backed_curriculum()
-    ensure_professional_assessment_banks()
+    ensure_professional_assessment_banks_v2()
     _app.main()
